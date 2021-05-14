aec5a76328f8a0ee32cf329a6bf9227ff9ba0b91
manmaed <manmaed18@gmail.com>
2021-05-14 17:15:30 +0100

    This Will Fix Builds LOL

---
+++ src/main/java/net/manmaed/cutepuppymod/libs/Refs.java
@@ -0,0 +1,10 @@
+package net.manmaed.cutepuppymod.libs;
+
+/**
+ * Created by manmaed on 09/04/2021.
+ */
+public class Refs {
+    public static final String  id = "cutepuppymod";
+    public static final String Register = id + ":";
+    public static final String LOG = ":[CutePuppyMod]:";
+}
