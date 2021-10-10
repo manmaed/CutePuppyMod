f6bbe7082cb0a0a5edc0360d41bde392890210ba
manmaed <manmaed18@gmail.com>
2021-10-10 15:51:01 +0100

    Work on ToolMaterials

--- src/main/java/net/manmaed/cutepuppymod/materials/ToolMaterials.java
+++ src/main/java/net/manmaed/cutepuppymod/materials/ToolMaterials.java
@@ -1,42 +1,37 @@
 package net.manmaed.cutepuppymod.materials;
 
 import net.manmaed.cutepuppymod.items.CPPuppyDrops;
-import net.minecraft.item.IItemTier;
-import net.minecraft.item.Items;
-import net.minecraft.item.crafting.Ingredient;
-import net.minecraft.util.LazyValue;
+import net.minecraft.util.LazyLoadedValue;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.crafting.Ingredient;
 
 import java.util.function.Supplier;
 
 /**
- * Complete copy paste from {@link net.minecraft.item.ItemTier}
+ * Complete copy paste from {@link net.minecraft.world.item.Tiers}
  */
-public enum ToolMaterials implements IItemTier {
+public enum ToolMaterials implements Tier {
 
     BAN(3, 3000, 2.0F, 20F, 0, () -> {
-        return Ingredient.fromItems(Items.AIR);
-    }),
-    RED_CORE(2, 450, 8.0F, 2.0F, 18, () -> {
-        return Ingredient.fromItems(CPPuppyDrops.redcore);
-    }),
-    BLUE_CORE(2, 450, 8.0F, 2.0F, 18, () -> {
-        return Ingredient.fromItems(CPPuppyDrops.bluecore);
-    }),
-    YELLOW_CORE(2, 450, 8.0F, 2.0F, 18, () -> {
-        return Ingredient.fromItems(CPPuppyDrops.yellowcore);
-    }),
-    PURPLE_CORE(2, 450, 8.0F, 2.0F, 18, () -> {
-        return Ingredient.fromItems(CPPuppyDrops.purplecore);
-    }),
-    GREEN_CORE(2, 450, 8.0F, 2.0F, 18, () -> {
-        return Ingredient.fromItems(CPPuppyDrops.greencore);
+        return Ingredient.of(Items.AIR);
+    }), RED_CORE(2, 450, 8.0F, 2.0F, 18, () -> {
+        return Ingredient.of(CPPuppyDrops.RED_CORE.get());
+    }), BLUE_CORE(2, 450, 8.0F, 2.0F, 18, () -> {
+        return Ingredient.of(CPPuppyDrops.BLUE_CORE.get());
+    }), YELLOW_CORE(2, 450, 8.0F, 2.0F, 18, () -> {
+        return Ingredient.of(CPPuppyDrops.YELLOW_CORE.get());
+    }), PURPLE_CORE(2, 450, 8.0F, 2.0F, 18, () -> {
+        return Ingredient.of(CPPuppyDrops.PURPLE_CORE.get());
+    }), GREEN_CORE(2, 450, 8.0F, 2.0F, 18, () -> {
+        return Ingredient.of(CPPuppyDrops.GREEN_CORE.get());
     });
     private final int harvestLevel;
     private final int maxUses;
     private final float efficiency;
     private final float attackDamage;
     private final int enchantability;
-    private final LazyValue<Ingredient> repairMaterial;
+    private final LazyLoadedValue<Ingredient> repairMaterial;
 
     ToolMaterials(int harvestLevelIn, int maxUsesIn, float efficiencyIn, float attackDamageIn, int enchantabilityIn, Supplier<Ingredient> repairMaterialIn) {
         this.harvestLevel = harvestLevelIn;
@@ -44,30 +39,30 @@ public enum ToolMaterials implements IItemTier {
         this.efficiency = efficiencyIn;
         this.attackDamage = attackDamageIn;
         this.enchantability = enchantabilityIn;
-        this.repairMaterial = new LazyValue<>(repairMaterialIn);
+        this.repairMaterial = new LazyLoadedValue<>(repairMaterialIn);
     }
 
-    public int getMaxUses() {
+    public int getUses() {
         return this.maxUses;
     }
 
-    public float getEfficiency() {
+    public float getSpeed() {
         return this.efficiency;
     }
 
-    public float getAttackDamage() {
+    public float getAttackDamageBonus() {
         return this.attackDamage;
     }
 
-    public int getHarvestLevel() {
+    public int getLevel() {
         return this.harvestLevel;
     }
 
-    public int getEnchantability() {
+    public int getEnchantmentValue() {
         return this.enchantability;
     }
 
-    public Ingredient getRepairMaterial() {
-        return this.repairMaterial.getValue();
+    public Ingredient getRepairIngredient() {
+        return this.repairMaterial.get();
     }
 }
